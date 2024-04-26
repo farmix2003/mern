4a03f4363a7233a67f3f6f2106bfb87865b80653
Farmix2003 <ffarrux386@gmail.com>
2024-04-26 21:40:43 +0300

    changes

--- front/src/server/axios.js
+++ front/src/server/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
+    method: ["GET", "POST", "DELETE", "PUT"],
     baseURL: "https://inter-paint.vercel.app",
     withCredentials: true,
 });
