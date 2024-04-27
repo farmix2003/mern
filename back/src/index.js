31d700a6c4d1a313573c368269c7d9aea9758c4e
Farmix2003 <ffarrux386@gmail.com>
2024-04-27 22:15:03 +0300

    server

--- back/src/index.js
+++ back/src/index.js
@@ -2,33 +2,35 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import cors from 'cors'
 import dotenv from 'dotenv'
-import mysql2 from 'mysql2';
-
+import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken'
 import verifyToken from './auth.js'
 
 dotenv.config();
 
-
-
-const DB_HOST = 'localhost';
-const DB_NAME = 'users';
-const DB_PORT = 3307;
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-
-
-
-const connection = mysql2.createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    port: DB_PORT
+// MongoDB connection setup
+mongoose.connect('mongodb+srv://ffarrux386:KmXZwtslRIay6oel@cluster0.aveiyoe.mongodb.net/mern', {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
 });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// MongoDB Schema definition
+const userSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    registration_time: { type: Date, default: Date.now },
+    last_login_time: { type: Date, default: Date.now },
+    status: { type: String, default: 'active' }
+});
+const User = mongoose.model('users', userSchema);
 
 const app = express();
-const router = express.Router()
+const router = express.Router();
 app.use(bodyParser.json());
 const options = {
     origin: 'https://mern-front-brown.vercel.app',
@@ -36,9 +38,7 @@ const options = {
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
     credentials: true
 };
-
 app.use(cors(options));
-
 app.use(router);
 
 app.options('/api/users/get-users', cors(options))
@@ -53,25 +53,23 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: 'Server is running' });
 })
 
-app.get('/api/users/get-users', (req, res) => {
-
-    connection.query('select * from users', (error, results, fields) => {
-        if (error) {
-            return res.status(500).send({ error: 'Internal Server Error' });
-        }
-        res.status(200).json(results);
-    })
+app.get('/api/users/get-users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 const generateAccessToken = (user) => {
     return jwt.sign({ id: user.id, email: user.email, }, process.env.SECRET_KEY, { expiresIn: '1h' })
-
 }
 
 const generateRefreshToken = (user) => {
     return jwt.sign({ id: user.id, email: user.email, }, process.env.SECRET_KEY)
 }
-
 
 let refreshTokens = []
 let accessTokens = []
@@ -81,127 +79,94 @@ app.get('/api/token', (req, res) => {
     accessTokens.push(token)
 })
 
-router.post('/api/users/login', (req, res) => {
+router.post('/api/users/login', async (req, res) => {
     const { email, password } = req.body;
-
-    connection.query(
-        'SELECT * FROM users WHERE email = ?',
-        [email],
-        (error, results) => {
-            if (error) {
-                console.error('Error logging in user:', error);
-                return res.status(500).json({ success: false, error: error });
-            }
-
-            if (results.length === 0) {
-                return res.status(404).json({ success: false, error: 'Invalid email or password' });
-            }
-
-            const user = results[0];
-            if (user.status === 'blocked') {
-                return res.status(403).json({ success: false, error: 'Your account is blocked. Please contact support.' });
-            }
-
-            if (user.email === !email || user.password !== password) {
-                return res.status(404).json({ success: false, error: 'Invalid email or password' });
-            }
-
-            const accessToken = generateAccessToken(user)
-            const refreshToken = generateRefreshToken(user)
-            refreshTokens.push(refreshToken)
-            console.log("Successfully logged in")
-            return res.status(200).json({ success: true, message: 'Successfully logged in', accessToken, refreshToken });
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ success: false, error: 'Invalid email or password' });
         }
-    );
+        if (user.status === 'blocked') {
+            return res.status(403).json({ success: false, error: 'Your account is blocked. Please contact support.' });
+        }
+        if (user.password !== password) {
+            return res.status(404).json({ success: false, error: 'Invalid email or password' });
+        }
+        const accessToken = generateAccessToken(user)
+        const refreshToken = generateRefreshToken(user)
+        refreshTokens.push(refreshToken)
+        console.log("Successfully logged in")
+        return res.status(200).json({ success: true, message: 'Successfully logged in', accessToken, refreshToken });
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(500).json({ success: false, error: 'Failed to login user' });
+    }
 });
 
-
-router.post('/api/users/register', (req, res) => {
+router.post('/api/users/register', async (req, res) => {
     const { name, email, password } = req.body;
-    const registration_time = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    const last_login_time = registration_time;
-    const status = 'active';
-    connection.query(
-        'INSERT INTO users (id, name, email, password, last_login_time, registration_time, status) VALUES (?, ?, ?, ?, ?, ?, ?)',
-        [null, name, email, password, last_login_time, registration_time, status],
-        (err, result) => {
-            if (err) {
-                console.error('Error registering user:', err);
-                res.status(500).json({ error: 'Failed to register user' });
-            } else {
-                console.log('User registered successfully:', result);
+    try {
+        const newUser = new User({
+            _id: new mongoose.Types.ObjectId(),
+            name: name,
+            email: email,
+            password: password
+        });
+
+        newUser.save()
+            .then(result => {
+                console.log('User registered successfully');
                 res.status(200).json({ message: 'User registered successfully' });
-            }
-        }
-    );
-
+            })
+            .catch(error => {
+                console.error('Error registering user:', error);
+                res.status(500).json({ error: 'Failed to register user' });
+            });
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({ error: 'Failed to register user' });
+    }
 });
 
-router.delete('/api/users/delete', verifyToken, (req, res) => {
+router.delete('/api/users/delete', verifyToken, async (req, res) => {
     const { ids } = req.body;
-    if (!ids || !Array.isArray(ids)) {
-        return res.status(400).json({ error: 'Invalid user IDs provided' });
+    try {
+        await User.deleteMany({ _id: { $in: ids } });
+        console.log('Users deleted successfully');
+        res.status(200).json({ message: 'Users deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting users:', error);
+        res.status(500).json({ error: 'Failed to delete users' });
     }
-
-    connection.query(
-        'DELETE FROM users WHERE id IN (?)',
-        [ids],
-        (err, result) => {
-            if (err) {
-                console.error('Error deleting users:', err);
-                return res.status(500).json({ error: 'Failed to delete users' });
-            }
-            console.log('Users deleted successfully:', result.affectedRows);
-            res.status(200).json({ message: 'Users deleted successfully' });
-        }
-    );
 });
 
-
-router.put('/api/users/block', verifyToken, (req, res) => {
+router.put('/api/users/block', verifyToken, async (req, res) => {
     const { ids } = req.body;
-    if (!ids || !Array.isArray(ids)) {
-        return res.status(400).json({ error: 'Invalid user IDs provided' });
+    try {
+        await User.updateMany({ _id: { $in: ids } }, { status: 'blocked' });
+        console.log('Users blocked successfully');
+        res.status(200).json({ message: 'Users blocked successfully' });
+    } catch (error) {
+        console.error('Error blocking users:', error);
+        res.status(500).json({ error: 'Failed to block users' });
     }
-    connection.query(
-        'UPDATE users SET status =? WHERE id IN (?)',
-        ['blocked', ids],
-        (err, result) => {
-            if (err) {
-                console.error('Error blocking users:', err);
-                res.status(500).json({ error: 'Failed to block users' });
-            } else {
-                console.log('Users blocked successfully:', result);
-                res.status(200).json({ message: 'Users blocked successfully' });
-            }
-        }
-    )
-})
+});
 
-router.put('/api/users/unblock', verifyToken, (req, res) => {
+router.put('/api/users/unblock', verifyToken, async (req, res) => {
     const { ids } = req.body;
-    if (!ids || !Array.isArray(ids)) {
-        return res.status(400).json({ error: 'Invalid user IDs provided' });
+    try {
+        await User.updateMany({ _id: { $in: ids } }, { status: 'active' });
+        console.log('Users unblocked successfully');
+        res.status(200).json({ message: 'Users unblocked successfully' });
+    } catch (error) {
+        console.error('Error unblocking users:', error);
+        res.status(500).json({ error: 'Failed to unblock users' });
     }
-    connection.query(
-        'UPDATE users SET status =? WHERE id IN (?)',
-        ['active', ids],
-        (err, result) => {
-            if (err) {
-                console.error('Error unblocking users:', err);
-                res.status(500).json({ error: 'Failed to unblock users' });
-            } else {
-                console.log('Users unblocked successfully:', result);
-                res.status(200).json({ message: 'Users unblocked successfully' });
-            }
-        }
-    )
-})
-
+});
 
 router.post('/api/users/logout', verifyToken, (req, res) => {
     res.status(200).json({ success: true, message: 'User logged out successfully' });
-})
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
